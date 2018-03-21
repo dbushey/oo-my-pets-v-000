d998161ddfc7295ded9b86d3a6886e4d75029ae6
Daiane Bushey <daianebushey@gmail.com>
2018-03-21 19:18:37 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -65,6 +65,7 @@ class Owner
     hash_all_pets = self.pets
     hash_all_pets.each do |key,val|
       hash_all_pets[key].map {|k| k.mood = "nervous"}
+      hash_all_pets[key].map {|k| k = []}
     end
     binding.pry
 
